883057be12cfdba2bc2e03791b175209d60b85c7
jezao <jefferson.ws@gmail.com>
2017-03-28 18:59:09 -0300

    Created jtimer.js

---
+++ jtimer.js
@@ -0,0 +1,29 @@
+function jTimer(_target,_mask=null)
+{
+    var _hours = _minutes = _seconds = 0;
+    var _mask    = (_mask == null) ? '[HH]:[MM]:[SS]' : _mask;
+
+    setInterval(function(){
+        _seconds++;
+        if( _seconds > 59 )
+        {
+            _minutes++;
+            _seconds = 0;
+        }
+
+        if( _minutes > 59)
+        {
+            _minutes = 0;
+            _hours++;
+        }
+
+        strsec = ( _seconds <= 9 ) ? "0"+_seconds : _seconds;
+        strmin = ( _minutes <= 9 ) ? "0"+_minutes : _minutes;
+        strhra = ( _hours   <= 9 ) ? "0"+_hours : _hours;
+
+        _target.innerHTML = _mask.replace('[HH]',(( _hours   <= 9 ) ? "0"+_hours : _hours))
+                                 .replace('[MM]',(( _minutes <= 9 ) ? "0"+_minutes : _minutes))
+                                 .replace('[SS]',( _seconds <= 9 ) ? "0"+_seconds : _seconds);
+
+    },1000);
+}
